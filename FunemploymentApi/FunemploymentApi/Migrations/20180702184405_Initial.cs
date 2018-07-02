bcc8b8f023408ab0516168312a2d64390c024ef4
Max <gusstorm132@gmail.com>
2018-07-02 12:51:33 -0700

    Have Databases and Table Controllers set up

---
+++ FunemploymentApi/FunemploymentApi/Migrations/20180702184405_Initial.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FunemploymentApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BehaviorQuestions",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Content = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BehaviorQuestions", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TechnicalQuestions",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Content = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TechnicalQuestions", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BehaviorQuestions");
+
+            migrationBuilder.DropTable(
+                name: "TechnicalQuestions");
+        }
+    }
+}
---
+++ FunemploymentApi/FunemploymentApi/Models/TechnicalQuestion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunemploymentApi.Models
+{
+    public class TechnicalQuestion
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+        //public string SuggestedAnswer { get; set; }
+        //public List<string> Companies { get; set; }
+    }
+}
